1b868d387604860ca733814677acf24e9f69a572
p1timmy <pkmntrainertimmy@gmail.com>
2024-11-30 16:16:47 -0700

    Make sure posts in database aren't duplicated
    This can sometimes happen within a couple minutes of starting the
    server when starts catching up to the latest posts using
    backfilled data

--- server/database.py
+++ server/database.py
@@ -10,7 +10,7 @@ class BaseModel(peewee.Model):
 
 class Post(BaseModel):
     uri = peewee.CharField(index=True)
-    cid = peewee.CharField()
+    cid = peewee.CharField(unique=True)
     author_did = peewee.CharField()
     reply_parent = peewee.CharField(null=True, default=None)
     reply_root = peewee.CharField(null=True, default=None)
