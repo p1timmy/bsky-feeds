b0104427e6453007fab8018ad52f7a68125896cd
p1timmy <pkmntrainertimmy@gmail.com>
2025-01-03 21:41:58 -0700

    Improve `publish_feed.py`
    - Check for missing required variables
    - Check if hostname is a valid domain, prevents the user from breaking
      feeds due to setting the feed DID to domains not used in the
      internet like `localhost`
    - Print out what's actually happening before the feed is published
    - Some minor refactoring and typing improvements

--- publish_feed.py
+++ publish_feed.py
@@ -4,8 +4,11 @@
 
 import os
 
+import validators
 from atproto import Client, models
+from click import secho, style
 from dotenv import load_dotenv
+from tld import get_tld, update_tld_names
 
 load_dotenv()
 
@@ -32,30 +35,58 @@ DISPLAY_NAME: str = os.environ.get("DISPLAY_NAME")
 
 # (Optional) A description of your feed
 # Ex: Top trending content from the whole network
-DESCRIPTION: str = os.environ.get("DESCRIPTION")
+DESCRIPTION: str | None = os.environ.get("DESCRIPTION")
 
 # (Optional) The path to an image to be used as your feed's avatar
 # Ex: ./path/to/avatar.jpeg
-AVATAR_PATH: str = os.environ.get("AVATAR_PATH")
+AVATAR_PATH: str | None = os.environ.get("AVATAR_PATH")
 
 # (Optional). Only use this if you want a service did different from did:web
-SERVICE_DID: str = os.environ.get("SERVICE_DID")
+SERVICE_DID: str | None = os.environ.get("SERVICE_DID")
+
+
+def check_params():
+    required_params = {
+        "HANDLE": HANDLE,
+        "PASSWORD": PASSWORD,
+        "HOSTNAME": HOSTNAME,
+        "RECORD_NAME": RECORD_NAME,
+        "DISPLAY_NAME": DISPLAY_NAME,
+    }
+    missing_params = [item[0] for item in required_params.items() if not item[1]]
+    if missing_params:
+        raise RuntimeError(
+            "Missing required parameters (add them to your .env file):"
+            f" {', '.join(missing_params)}"
+        )
+
+    update_tld_names(fail_silently=True)
+    if (
+        validators.domain(HOSTNAME) is not True
+        or get_tld(HOSTNAME, fail_silently=True, fix_protocol=True) is None
+    ):
+        raise RuntimeError(
+            "HOSTNAME must be set to a valid internet domain for the feed to work"
+        )
 
 
 def main():
+    check_params()
+
     client = Client()
+    secho(f"Logging in to Bluesky as @{HANDLE}...")
     client.login(HANDLE, PASSWORD)
 
-    feed_did = SERVICE_DID
-    if not feed_did:
-        feed_did = f"did:web:{HOSTNAME}"
+    feed_did = SERVICE_DID or f"did:web:{HOSTNAME}"
 
     avatar_blob = None
     if AVATAR_PATH:
         with open(AVATAR_PATH, "rb") as f:
+            secho("Uploading feed avatar...")
             avatar_data = f.read()
             avatar_blob = client.upload_blob(avatar_data).blob
 
+    secho("Sending feed info...")
     response = client.com.atproto.repo.put_record(
         models.ComAtprotoRepoPutRecord.Data(
             repo=client.me.did,
@@ -71,8 +102,8 @@ def main():
         )
     )
 
-    print("Successfully published!")
-    print("Feed URI:", response.uri)
+    secho("Successfully published!", fg="green")
+    secho(f"Feed URI: {style(response.uri, underline=True)}", bold=True)
 
 
 if __name__ == "__main__":
