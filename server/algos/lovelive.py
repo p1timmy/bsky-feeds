29089a786b94701a65b6f8fee68c2a1ad60d063f
p1timmy <pkmntrainertimmy@gmail.com>
2025-02-08 18:39:09 -0700

    A couple additions to LoveLive! exclude regex and ignore users list
    - Add a couple NSFW keywords to exclude regex
    - Add a NSFW account, trending bot, article summary account to ignore
      users list

--- server/algos/lovelive.py
+++ server/algos/lovelive.py
@@ -187,7 +187,8 @@ EXCLUDE_RE = re.compile(
     re.IGNORECASE | re.MULTILINE,
 )
 NSFW_KEYWORDS_RE = re.compile(
-    r"\b(hentai|futanari|p(enis|orn)|dildo|nsfw|bds&?m)", re.IGNORECASE
+    r"\b(hentai|futanari|p(enis|orn)|dildo|n(sfw|ude)|bds&?m|cum(ming)?\b)",
+    re.IGNORECASE,
 )
 
 # Prepopulate user list with only @lovelivenews.bsky.social just in case loading from
