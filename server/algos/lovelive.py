7b73aacd63f92ebd196e32d89c30edcf2aa98a85
p1timmy <pkmntrainertimmy@gmail.com>
2025-02-28 09:18:51 -0700

    Make separate regex for LoveLive! character names in romaji
    Should prevent many of the false positives with names involved such
    as mentions of random users using any of the names in their handles
    and phrases/words containing a substring of any of the names
    (especially Keke Tang's) from being added to the LoveLive!Sky feed
    
    Originally was gonna put "([^@]|^)" in front of every single pattern
    for each character name but that makes the main regex noticeably
    slower and harder to maintain

--- server/algos/lovelive.py
+++ server/algos/lovelive.py
@@ -20,20 +20,12 @@ LOVELIVE_RE = re.compile(
     # Love Live! School Idol Project
     # NOTE: Printemps, lily white, BiBi not included due to too many false positives
     r"音ノ木坂?|otonokizaka|[μµ]['’‘`´′]s|にこりんぱな|nicorinpana|"
-    r"高坂\s?穂乃果|honoka\s?kou?saka|kou?saka\s?honoka|"
-    r"絢瀬\s?絵里|([^@]|^)ayase\s?eli|eli\s?ayase|エリーチカ|\belichika\b|"
-    r"南\s?ことり|minami\s?kotori|kotori\s?minami|kotobirb|"
-    r"園田\s?海未|sonoda\s?umi|umi\s?sonoda|"
-    r"星空\s?凛|hoshizora\s?rin|rin\s?hoshizora|金曜凛ちゃんりんりんりん|"
-    r"西木野\s?真姫|nishikino\s?maki|maki\s?nishikino|"
-    r"東條\s?希|tou?jou?\s?nozomi|nozomi\s?tou?jou?|"
-    r"小泉\s?花陽|koizumi\s?hanayo|hanayo\s?koizumi|火曜日かよちゃん|"
-    r"矢澤\s?にこ|yazawa\s?nico|nico\s?yazawa|nico\snico\sni+\b|#niconiconi+\b|"
+    r"高坂\s?穂乃果|絢瀬\s?絵里|南\s?ことり|園田\s?海未|星空\s?凛|西木野\s?真姫|東條\s?希|"
+    r"小泉\s?花陽|矢澤\s?にこ|nico\snico\sni+\b|#niconiconi+\b|"
+    r"エリーチカ|\belichika\b|金曜凛ちゃんりんりんりん|火曜日かよちゃん|"
     r"snow\s?halation([^a-z\u00C0-\u024F\u1E00-\u1EFF]|\b)|"
     r"(^|[^a-z\u00C0-\u024F\u1E00-\u1EFF\-])a[-\u2010]rise([^a-z\u00C0-\u024F\u1E00-\u1EFF\-]|$)|"
-    r"綺羅\s?ツバサ|kira\s?tsubasa|tsubasa\s?kira|"
-    r"優木\s?あんじゅ|yuu?ki\s?anju|anju\s?yuu?ki|"
-    r"統堂\s?英玲奈|tou?dou?\s?erena|\berena\s?tou?dou?\b|"
+    r"綺羅\s?ツバサ|優木\s?あんじゅ|統堂\s?英玲奈|"
     # Love Live! Sunshine!!
     # NOTE: AZALEA not included due to too many false positives
     r"浦の星女?|uranohoshi|aq(ou|uo)rs|cyaron!?|guilty\s?kiss|"
@@ -42,58 +34,31 @@ LOVELIVE_RE = re.compile(
     r"([^a-z\u00C0-\u024F\u1E00-\u1EFF]|\b)aiscream|"
     r"幻(日のヨハネ|ヨハ)|\b(genjitsu ?no ?)?yohane\b(?!(.|\n)+shaman ?king)|"
     r"sunshine\sin\sthe\smirror|"
-    r"高海\s?千歌|takami\s?chika|chika\s?takami|"
-    r"桜内\s?梨子|sakurauchi\s?riko|riko\s?sakurauchi|"
-    r"松浦\s?果南|matsuu?ra\s?kanan|kanan\s?matsuu?ra|"
-    r"黒澤\s?(ダイヤ|ルビィ?)|kurosawa\s?(dia|ruby)|(dia|ruby)\s?kurosawa|"
+    r"高海\s?千歌|桜内\s?梨子|松浦\s?果南|黒澤\s?(ダイヤ|ルビィ?)|渡辺\s?曜|津島\s?善子|"
+    r"国木田\s?花丸|小原\s?鞠莉|"
     r"がんば(ルビ|るび)|ganbaruby|"
-    r"渡辺\s?曜|watanabe\s?you|you\s?watanabe|ヨーソロー?|ﾖｰｿﾛｰ?|\byousoro|"
-    r"津島\s?善子|tsushima\s?yoshiko|yoshiko\s?tsushima|堕天使ヨハネ|"
-    r"国木田\s?花丸|kunikida\s?hanamaru|hanamaru\s?kunikida|\bzuramaru|"
-    r"小原\s?鞠莉|ohara\s?mari|mari\s?ohara|"
     r"(永久|\beikyuu\s?)hours|"
     r"(?<!\bRT @)saint\s?snow|"
-    r"鹿角\s?(理亞|聖良)|kazuno\s?(le|sar)ah|(le|sar)ah\s?kazuno|"
+    r"鹿角\s?(理亞|聖良)|"
     # Nijigasaki
     r"虹ヶ咲|ニジガク|(アニ|エイ)ガサキ|(あに|えい)がさき|にじ(よん|ちず)|"
     r"(nij|an|e)igasaki|niji(chizu|gaku|yon)|a・zu・na|qu4rtz|"
     r"([^a-z\u00C0-\u024F\u1E00-\u1EFF]|\b)(diver"
     r" ?diva|r3birth)([^a-z\u00C0-\u024F\u1E00-\u1EFF]|\b)|"
-    r"高咲\s?侑|takasaki\s?yuu?|yuu?\s?takasaki|"
-    r"上原\s?歩夢|uehara\s?ayumu|ayumu\s?uehara|"
-    r"中須\s?かすみ|nakasu\s?kasumi|kasumi\s?nakasu|"
-    r"桜坂\s?しずく|ou?saka\s?shizuku|shizuku\s?ou?saka|"
-    r"朝香\s?果林|asaka\s?karin|karin\s?asaka|"
-    r"宮下\s?愛|miyashita\s?ai|ai\s?miyashita|"
-    r"近江\s?(彼方|遥)|konoe\s?(kanat|haruk)a|(kanat|haruk)a\s?konoe|"
-    r"優木\s?せつ菜|yuu?ki\s?setsuna|setsuna\s?yuu?ki|"
-    r"中川\s?菜々|nakagawa\s?nana|nana\s?nakagawa|"
-    r"エマ・?ヴェルデ|emma\s?verde|"
-    r"天王寺\s?璃奈|tennou?ji\s?rina|rina\s?tennou?ji|"
-    r"三船\s?栞子|mifune\s?shioriko|shioriko\s?mifune|"
-    r"ミア・?テイラー|\bmia\staylor\b|"
-    r"鐘\s?嵐珠|lanzhu\s?zhong|zhong\s?lanzhu|"
+    r"高咲\s?侑|上原\s?歩夢|中須\s?かすみ|桜坂\s?しずく|朝香\s?果林|宮下\s?愛|近江\s?(彼方|遥)|"
+    r"優木\s?せつ菜|中川\s?菜々|エマ・?ヴェルデ|天王寺\s?璃奈|三船\s?栞子|ミア・?テイラー|鐘\s?嵐珠|"
     # Love Live! Superstar!!
     r"([^a-z\u00C0-\u024F\u1E00-\u1EFF]|\b)liella(?! kelly)[!！]?|結ヶ丘|yuigaoka|"
     r"5yncri5e!?|kaleidoscore"
     r"|([^a-z\u00C0-\u024F\u1E00-\u1EFF]|\b)(?<!i )(?<!to"
     r" )catchu!?(?! later)([^a-z\u00C0-\u024F\u1E00-\u1EFF]|\b)|"
     r"トマカノーテ|tomakanote|スパスタ[3３]期|"
-    r"澁谷\s?かのん|([^@]|^)shibuya\s?kanon|kanon\s?shibuya|"
-    r"唐\s?可可|ク[ウゥ]ク[ウゥ]ちゃん?|\btang\s?keke|\bkeke\s?tang|"
-    r"嵐千\s?砂都|arashi\s?chisato|chisato\s?arashi|"
-    r"平安名\s?すみれ|heanna\s?sumire|sumire\s?heanna|"
-    r"葉月\s?恋|\bhazuki\s?ren\b|ren\s?hazuki|"
-    r"桜小路\s?きな子|sakurakoji\s?kinako|kinako\s?sakurakoji|"
-    r"米女\s?メイ|yoneme\s?mei|mei\s?yoneme|"
-    r"若菜\s?四季|wakana\s?shiki|shiki\s?wakana|"
-    r"鬼塚\s?(夏美|冬毬)|onitsuka\s?(natsum|tomar)i|(natsum|tomar)i\s?onitsuka|"
-    r"oninatsu|オニナッツ|"
-    r"ウィーン・?マルガレーテ|\bwien\s?margarete\b|"
+    r"澁谷\s?かのん|唐\s?可可|嵐千\s?砂都|平安名\s?すみれ|葉月\s?恋|桜小路\s?きな子|米女\s?メイ|"
+    r"若菜\s?四季|鬼塚\s?(夏美|冬毬)|ウィーン・?マルガレーテ|"
+    r"ク[ウゥ]ク[ウゥ]ちゃん?|oninatsu|オニナッツ|"
     r"sunny\s?pas(sion)?(\b|[^a-z\u00C0-\u024F\u1E00-\u1EFF])|"
     r"sunnypa(\b|[^a-z\u00C0-\u024F\u1E00-\u1EFF])|"
-    r"聖澤悠奈|hijirisawa\s?yuu?na|yuu?na\s?hijirisawa|"
-    r"柊\s?摩央|hiiragi\s?mao|mao\s?hiiragi|"
+    r"聖澤悠奈|柊\s?摩央|"
     # Link! Like! Love Live! / Hasunosora
     # リンクラ but not リンクライン or katakana phrases with リンクラ character sequence
     r"(^|[^\u30a1-\u30f6\u30fc])リンクラ(?!イン)|"
@@ -101,16 +66,8 @@ LOVELIVE_RE = re.compile(
     r"cerise\sbouquet|スリーズブーケ|dollchestra(?!-art)|ドルケストラ|"
     r"mira-cra\spark!?|みらくらぱーく[!！]?|\bkahomegu\b|かほめぐ(♡じぇらーと)?|"
     r"るりのとゆかいなつづりたち|"
-    r"乙宗\s?梢|otomune\s?kozue|kozue\s?otomune|"
-    r"夕霧\s?綴理|yugiri\s?tsuzuri|tsuzuri\s?yugiri|"
-    r"藤島\s?慈|fujishima\s?megumi|megumi\s?fujishima|"
-    r"日野下\s?花帆|hinoshita\s?kaho|kaho\s?hinoshita|"
-    r"村野\s?さやか|murano\s?sayaka|sayaka\s?murano|"
-    r"大沢\s?瑠璃乃|osawa\s?rurino|rurino\s?osawa|"
-    r"百生\s?吟子|momose\s?ginko|ginko\s?momose|"
-    r"徒町\s?小鈴|kachimachi\s?kosuzu|kosuzu\s?kachimachi|"
-    r"安養寺\s?姫芽|anyoji\s?hime|\bhime\s?anyoji\b|"
-    r"大賀美沙知|ogami\s?sachi|sachi\s?ogami|"
+    r"乙宗\s?梢|夕霧\s?綴理|藤島\s?慈|日野下\s?花帆|村野\s?さやか|大沢\s?瑠璃乃|百生\s?吟子|"
+    r"徒町\s?小鈴|安養寺\s?姫芽|大賀美沙知|"
     # Love Live! Bluebird
     r"いきづらい部|イキヅライブ|"
     # Concerts
@@ -121,6 +78,75 @@ LOVELIVE_RE = re.compile(
 )
 SUKUFEST_RE = re.compile(
     r"(^|[^マ])スクフェス(?!札幌|大阪|福岡|神奈川|新潟|仙台|三河|沖縄)"
+)
+CHARACTER_NAMES = set(
+    {
+        # μ's
+        ("Honoka", "Kou?saka", False),  # First name, Last name, First-Last order only
+        ("Eli", "Ayase", False),
+        ("Kotori", "Minami", False),
+        ("Umi", "Sonoda", False),
+        ("Rin", "Hoshizora", False),
+        ("Maki", "Nishikino", False),
+        ("Nozomi", "Tou?jou?", False),
+        ("Hanayo", "Koizumi", False),
+        ("Nico", "Yazawa", False),
+        # A-RISE
+        ("Kira", "Tsubasa", False),
+        ("Anju", "Yuu?ki", False),
+        ("Erena", "Tou?dou?", False),
+        # Aqours
+        ("Chika", "Takami", False),
+        ("Riko", "Sakurauchi", False),
+        ("Kanan", "Matsuu?ra", False),
+        ("(Dia|Ruby)", "Kurosawa", False),
+        ("You", "Watanabe", False),
+        ("Yoshiko", "Tsushima", False),
+        ("Hanamaru", "Kunikida", False),
+        ("Mari", "Ohara", False),
+        # Saint Snow
+        ("(Le|Sar)ah", "Kazuno", False),
+        # Nijigasaki
+        ("Yuu?", "Takasaki", False),
+        ("Ayumu", "Uehara", False),
+        ("Kasumi", "Nakasu", False),
+        ("Shizuku", "Ou?saka", False),
+        ("Karin", "Asaka", False),
+        ("Ai", "Miyashita", False),
+        ("(Kanat|Haruk)a", "Konoe", False),
+        ("Setsuna", "Yuu?ki", False),
+        ("Nana", "Nakagawa", False),
+        ("Emma", "Verde", True),
+        ("Rina", "Tennou?ji", False),
+        ("Shioriko", "Mifune", False),
+        # Mia Taylor included in pattern builder due to so many false positives
+        ("Lanzhu", "Zhong", False),
+        # Liella
+        ("Kanon", "Shibuya", False),
+        ("Keke", "Tang", False),
+        ("Chisato", "Arashi", False),
+        ("Sumire", "Heanna", False),
+        ("Ren", "Hazuki", False),
+        ("Kinako", "Sakurakoji", False),
+        ("Mei", "Yoneme", False),
+        ("Shiki", "Wakana", False),
+        ("(Natsum|Tomar)i", "Onitsuka", False),
+        ("Wien", "Margarete", True),
+        # Sunny Passion
+        ("Yuu?na", "Hijirisawa", False),
+        ("Mao", "Hiiragi", False),
+        # Hasunosora
+        ("Kozue", "Otomune", False),
+        ("Tsuzuri", "Yugiri", False),
+        ("Megumi", "Fujishima", False),
+        ("Kaho", "Hinoshita", False),
+        ("Sayaka", "Murano", False),
+        ("Rurino", "Osawa", False),
+        ("Ginko", "Momose", False),
+        ("Kosuzu", "Kachimachi", False),
+        ("Hime", "Anyoji", False),
+        ("Sachi", "Ogami", False),
+    }
 )
 
 EXCLUDE_RE = re.compile(
@@ -219,6 +245,23 @@ dedicated_userlist_uri = config.LOVELIVE_INCLUDE_LIST_URI
 ignore_list_uri = config.LOVELIVE_IGNORE_LIST_URI
 
 
+def make_characters_pattern() -> re.Pattern:
+    patterns: list[str] = []
+    for name in CHARACTER_NAMES:
+        first, last, first_last_only = name
+        if not first_last_only:
+            patterns.append(f"{last} ?{first}")
+
+        patterns.append(f"{first} ?{last}")
+
+    return re.compile(
+        f"(?:^|[^@a-z])(?:{'|'.join(patterns)}|mia taylor)\\b", re.IGNORECASE
+    )
+
+
+CHARACTERS_EN_RE = make_characters_pattern()
+
+
 def filter(post: dict) -> bool:
     author = post["author"]
     if author in DEDICATED_USERS:
@@ -234,7 +277,8 @@ def filter(post: dict) -> bool:
     return not NSFW_KEYWORDS_RE.search(all_texts) and any(
         (
             LOVELIVE_NAME_EN_RE.search(all_texts) and not EXCLUDE_RE.search(all_texts),
-            SUKUFEST_RE.search(all_texts) and not "scrum" in all_texts.lower(),
+            SUKUFEST_RE.search(all_texts) and "scrum" not in all_texts.lower(),
             LOVELIVE_RE.search(all_texts),
+            CHARACTERS_EN_RE.search(all_texts),
         )
     )
