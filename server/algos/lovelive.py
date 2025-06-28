a5ac7705fa30f13558978a646a50ddaca9a7b8a4
p1timmy <pkmntrainertimmy@gmail.com>
2025-06-28 16:39:34 -0600

    Add posts containing "OurSIF" to LoveLive! feed

--- server/algos/lovelive.py
+++ server/algos/lovelive.py
@@ -83,7 +83,8 @@ LOVELIVE_RE = re.compile(
     # Concerts
     r"異次元フェス|ijigen\sfest?|#llsat_|"
     # Community stuff
-    r"\b(team )?onib(e|ased)\b|schoolido\.lu|idol\.st(?!/user/\d+)|#HasuTH_Tran",
+    r"\b(team )?onib(e|ased)\b|schoolido\.lu|idol\.st(?!/user/\d+)|#HasuTH_Tran|"
+    r"([^a-z]|\b)OurSIF([^a-z]|$)",
     re.IGNORECASE,
 )
 SUKUFEST_RE = re.compile(
