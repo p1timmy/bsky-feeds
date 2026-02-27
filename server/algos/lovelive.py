1f8cdf5b973a7d2a490a96fab0366da8f6b87274
p1timmy <pkmntrainertimmy@gmail.com>
2026-02-27 14:33:28 -0700

    Filter out "happens to love live(r)"
    Massive wave of "love liver" false positive posts happened during dead
    periods of Feb. 28th, 2026 JST (Kosuzu's birthday), 1 of them containing
    that phrase managed to sneak into the LoveLive!Sky feed

--- server/algos/lovelive.py
+++ server/algos/lovelive.py
@@ -243,11 +243,11 @@ EXCLUDE_RE = re.compile(
     r"[a-z]{3,}(?<!a)(?<!e)s( that)?|my .+and [a-z]+[a-z]|"
     r"anyone( else['’]s|( else)?( (o(ver|ut) )?t?here)?)?)"
     # - *ly/also/always/bloody/can't/cannot/(sure) do/does/don't (but not "don't do")/
-    #   even/f*king/hecking/just/lowkey/still/(came/come/grew/have/happened/use(d)/tend)
-    #   to/too/will/would('ve)/... and
+    #   even/f*king/hecking/just/lowkey/still/(came/come/grew/have/happen(ed/s)/use(d)/
+    #   tend) to/too/will/would('ve)/... and
     r"(( ?,)? ([a-z]+ly|al(so|ways)|bloody|can(['’]|((['’]?t)? )?no)t|(sure )?do|does|"
     r"don['’]?t(?! do\b)|even|(f[a-z]+|hec)ki?n[g'’]?|just|lowkey|(st|w)ill|"
-    r"((c[ao]me|ha(ve|ppen(ed)?)|used?|grew) t|s|t(o|end t))o|would(['’]ve)?|"
+    r"((c[ao]me|ha(ve|ppen(ed|s)?)|used?|grew) t|s|t(o|end t))o|would(['’]ve)?|"
     r"[a-z]+[a-z] (and|&)))*"
     # - love live [something]/love liver(s)/love Live (as in a band named LĪVE, Ableton
     #   Live music software, or typo of "love life")
@@ -565,6 +565,7 @@ EXCLUDE_RE = re.compile(
     # - "Sexy Love" live
     # - "Show Me Love" live (usually song by Robin S.)
     # - Simon Love live (some random British artist with an official Bluesky account)
+    # - "Skinny Love" live (song by Bon Iver)
     # - "Some Kinda Love" live (song by The Velvet Underground)
     # - "Soul Love" live (song by David Bowie)
     # - "Spiritual Love" live (usually song by Urban Species)
